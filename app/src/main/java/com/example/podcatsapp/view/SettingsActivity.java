aa90fc40aad82036d5514efe3dea9324a4ddac02
AdrianGarciaCaballero <144772061+AdrianGarciaCaballero@users.noreply.github.com>
2024-10-29 13:19:34 +0100

    cositas

--- app/src/main/java/com/example/podcatsapp/view/SettingsActivity.java
+++ app/src/main/java/com/example/podcatsapp/view/SettingsActivity.java
@@ -1,21 +1,39 @@
 package com.example.podcatsapp.view;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.podcatsapp.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Switch switchNotifications;
+    private Switch switchDarkMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+       // switchNotifications = findViewById(R.id.switch_notifications);
+       // switchDarkMode = findViewById(R.id.switch_dark_mode);
+
+     /*
+        switchNotifications.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // codigo notificacines
+        });
+
+        switchDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // codigo modo oscuro
+        });*/
+    }
+
+    public void editProfile(View view) {
+        // codigo editar perfil
+    }
+
+    public void changePassword(View view) {
+        // codigo cambiar contraseña
     }
 }
